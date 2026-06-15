7845447c4f8e6d9824e1fd8ace71f828ef94242d
Matthew Swanberg <matt20swanberg@gmail.com>
2026-06-14 20:46:14 -0400

    Add coffee search functionality

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import { coffees } from "../data/coffeeData";
 import CoffeeCard from "../components/CoffeeCard.jsx";
 
 function ShopPage() {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredCoffees = coffees.filter((coffee) =>
+        coffee.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
     return (
         <div className="shop-page">
-            {coffees.map((coffee) => (
-                < CoffeeCard
-                    key={coffee.id}
-                    coffee={coffee}
-
-                />
-            )
-            )
+            <input type="text" placeholder="Search coffee by name" className="search-input" value={searchTerm} onChange={handleSearch} />
+            {filteredCoffees.length === 0 ? (
+                <p className="no-search-results">No results found</p>
+            ) :
+                (filteredCoffees.map((coffee) => (
+                    < CoffeeCard
+                        key={coffee.id}
+                        coffee={coffee}
+                    />
+                ))
+                )
             }
         </div>
     )
